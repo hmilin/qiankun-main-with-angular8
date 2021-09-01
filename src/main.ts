866a07db400164db9f99204485bc6736ddea1bf5
hmilin <hmilin@163.com>
2021-09-02 00:12:56 +0800

    feat:  add qiankun

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { registerMicroApps, start} from 'qiankun';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -7,6 +8,17 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
+
+registerMicroApps([
+  {
+    name: 'reactApp',
+    entry: '//localhost:3000',
+    container: '#container',
+    activeRule: '/app-react',
+  },
+]);
+// start qiankun
+start();
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
